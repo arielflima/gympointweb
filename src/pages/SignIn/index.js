53fe3ef796c2428d6ff3f470aef08d8c3f9e1b1b
Ariel V Lima <arielflima@gmail.com>
2019-12-20 17:11:03 -0300

    configuring eslint-babel-import to relatives paths and starting app

---
+++ src/pages/SignIn/index.js
@@ -0,0 +1,22 @@
+import React from 'react';
+import { Form, Input } from '@rocketseat/unform';
+import { Link } from 'react-router-dom';
+import * as Yup from 'yup';
+
+import { Container } from './styles';
+import logo from '~/assets/logo.svg';
+
+const schema = Yup.object().shape({
+  email: Yup.string()
+    .email('Insira seu e-mail')
+    .required('O e-mail é obrigatorio!'),
+  password: Yup.string().required('A senha é obrigatória!'),
+});
+
+export default function SignIn() {
+  return (
+    <>
+      <img src={logo} alt="GymPoint" />
+    </>
+  );
+}
