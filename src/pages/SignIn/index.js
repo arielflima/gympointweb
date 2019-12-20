66756443789e2e2315a673a0375312fe78fe1649
Ariel V Lima <arielflima@gmail.com>
2019-12-20 18:51:38 -0300

    style finished signIn page

--- src/pages/SignIn/index.js
+++ src/pages/SignIn/index.js
@@ -1,24 +1,35 @@
 import React from 'react';
-/*
+
 import { Form, Input } from '@rocketseat/unform';
 import { Link } from 'react-router-dom';
 import * as Yup from 'yup';
-*/
 
-// import { Container } from './styles';
+import { Container, Content } from './styles';
 import logo from '~/assets/logo.svg';
-/*
+
 const schema = Yup.object().shape({
   email: Yup.string()
     .email('Insira seu e-mail')
     .required('O e-mail é obrigatorio!'),
   password: Yup.string().required('A senha é obrigatória!'),
 });
-*/
+
 export default function SignIn() {
   return (
     <>
-      <img src={logo} alt="GymPoint" />
+      <Container>
+        <Content>
+          <img src={logo} alt="GymPoint" />
+          <Form schema={schema}>
+            <strong>SEU E-MAIL</strong>
+            <Input name="email" type="email" placeholder="exemplo@email.com" />
+            <strong>SUA SENHA</strong>
+            <Input name="password" type="password" placeholder="***********" />
+
+            <button type="submit">Entrar no sistema</button>
+          </Form>
+        </Content>
+      </Container>
     </>
   );
 }
---
+++ src/pages/SignIn/styles.js
@@ -0,0 +1,55 @@
+import styled from 'styled-components';
+
+export const Container = styled.div`
+  height: 100%;
+  width: 100%;
+  background: #ee4d64;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  margin: 0;
+  border: 0;
+`;
+
+export const Content = styled.div`
+  width: 100%;
+  height: 100%;
+  max-width: 360px;
+  max-height: 448px;
+  text-align: center;
+  background: #f2f2f2;
+  padding: 30px;
+
+  img {
+    margin-top: 15px;
+  }
+
+  form {
+    text-align: start;
+    display: flex;
+    flex-direction: column;
+    margin: 30px 0;
+
+    strong {
+      font-size: 11px;
+    }
+
+    input {
+      margin-top: 5px;
+      padding: 10px;
+      margin-bottom: 20px;
+      border-radius: 4px;
+      border: 1px solid #dddddd;
+      color: #dddddd;
+    }
+
+    button {
+      padding: 10px;
+      background: #ee4d64;
+      border-radius: 4px;
+      font-weight: bold;
+      color: #fff;
+      border: 0 solid;
+    }
+  }
+`;
